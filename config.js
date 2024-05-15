57421784e8ff29659675e07b7d6a769519f2b35f
HectorRussia <Ponkrit.wo@hotmail.com>
2024-05-15 19:51:19 +0700

    edit config

---
+++ config.js
@@ -0,0 +1,9 @@
+const constants = {
+    openWeatherMap: 
+    {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "f73f410ef0f4036f3bac8365a14d546d"
+    }
+}
+
+module.exports = constants;
